9f010b55fed26ba4768023724fdbef8dfb876599
Emily CJones <emilycjones@college.harvard.edu>
2018-10-01 15:33:33 -0400

    Initial Commit. Error Handling Finished.

---
+++ Employee_Manager.rb
@@ -0,0 +1,13 @@
+class Employee
+
+  def initialize(name, title, salary, bonus)
+    @name = name
+    @title = title
+    @salary = salary
+    @bonus = bonus 
+  end
+
+
+
+
+end #end of the employee class
